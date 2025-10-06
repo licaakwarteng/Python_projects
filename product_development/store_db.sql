510246e0edf3860960683b81d26f67675e3653cc
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-10-06 12:45:17 +0000

    An inventory management api

---
+++ product_development/store_db.sql
@@ -0,0 +1,7 @@
+-- This database is created for the inventory management api
+DROP DATABASE if exists store_db;
+CREATE DATABASE store_db;
+USE store_db;
+
+-- After backend migrations, view tables created
+SHOW TABLES;
